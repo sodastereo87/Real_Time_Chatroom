1dd52eb152e8e2548e3ebdcdd96fde568e70b2ae
sodastereo <f_er_@hotmail.com>
2019-09-16 02:49:08 -0400

    collection added

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,6 +3,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat(message) {
         // format chat object
@@ -19,23 +20,46 @@ class Chatroom {
     }
     // setting up a real time listener to get new chats
     getChats(callback) {
-        this.chats
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
             .onSnapshot(snapshot => {
                 snapshot.docChanges().forEach(change => {
-                    if (change.type === 'added'){
+                    if (change.type === 'added') {
                         // update the ui
                         callback(change.doc.data());
                     }
                 });
             });
     }
+    // update username
+    updateName(username) {
+        this.username = username;
+    }
+    // update room
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub) {
+            this.unsub();
+        }
+    }
 }
 
-const chatroom = new Chatroom('gaming', 'shaun');
+const chatroom = new Chatroom('general', 'shaun');
 chatroom.getChats((data) => {
     console.log(data);
+});
 
-})
+setTimeout(() => {
+    chatroom.updateRoom('gaming');
+    chatroom.updateName('yoshi');
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat('hello');
+}, 3000);
+
 // chatroom.addChat('hello')
 // .then(() => console.log('chat added'))
 // // console log any error
