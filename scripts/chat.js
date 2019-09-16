ac5ae891abb03dcd1dbeaee3ddabc491c44357cc
sodastereo <f_er_@hotmail.com>
2019-09-16 02:17:17 -0400

    real time set up

--- scripts/chat.js
+++ scripts/chat.js
@@ -17,14 +17,29 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    // setting up a real time listener to get new chats
+    getChats(callback) {
+        this.chats
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added'){
+                        // update the ui
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
+chatroom.getChats((data) => {
+    console.log(data);
 
-chatroom.addChat('hello')
-.then(() => console.log('chat added'))
-// console log any error
-.catch(err => console.log(err));
+})
+// chatroom.addChat('hello')
+// .then(() => console.log('chat added'))
+// // console log any error
+// .catch(err => console.log(err));
 
 
 
